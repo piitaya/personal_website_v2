9b89bfc79e3e88adc039d8cb2ed3bd69f9f6f114
Bottein Paul <paul.bottein@gmail.com>
2016-01-27 18:53:42 +0100

    Indent using tab

--- client/app/app.routes.js
+++ client/app/app.routes.js
@@ -1,23 +1,23 @@
 (function () {
-    'use strict';
-    angular
-        .module('app')
-        .config(config);
+	'use strict';
+	angular
+		.module('app')
+		.config(config);
 
-    function config($stateProvider, $urlRouterProvider, $locationProvider) {
-        $locationProvider.html5Mode(true);
+	function config($stateProvider, $urlRouterProvider, $locationProvider) {
+		$locationProvider.html5Mode(true);
 
-        $stateProvider
-            .state ('home', {
-                url: '/',
-                templateUrl: 'app/components/home/home.html',
-                controller: 'HomeController',
-                controllerAs: 'vm'
-            });
+		$stateProvider
+			.state ('home', {
+				url: '/',
+				templateUrl: 'app/components/home/home.html',
+				controller: 'HomeController',
+				controllerAs: 'vm'
+			});
 
-        $urlRouterProvider.otherwise( function($injector) {
-            var $state = $injector.get("$state");
-            $state.go('home');
-        });
-    }
+		$urlRouterProvider.otherwise( function($injector) {
+			var $state = $injector.get("$state");
+			$state.go('home');
+		});
+	}
 })();
--- server/models/user.js
+++ server/models/user.js
@@ -5,18 +5,18 @@ var Schema = mongoose.Schema;
 
 // define the schema for our user model
 var userSchema = Schema({
-    email        : String,
-    password     : String
+	email: String,
+	password: String
 });
 
 // generating a hash
 userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 // checking if password is valid
 userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
+	return bcrypt.compareSync(password, this.password);
 };
 
 // create the model for users and expose it to our app
