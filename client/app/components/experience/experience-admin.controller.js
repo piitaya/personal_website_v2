27cf2aca8bec1020708fe558a6c45f9e41da76b2
Bottein Paul <paul.bottein@gmail.com>
2016-02-02 00:19:11 +0100

    Add create function and modal

--- client/app/components/experience/experience-admin.controller.js
+++ client/app/components/experience/experience-admin.controller.js
@@ -9,23 +9,34 @@
 	function ExperienceAdminController(experienceService) {
 		var vm = this;
 		
-		vm.experiences = experienceService.query(); // returns all the pokemons
+		vm.experiences = [];
+		vm.newExperience = {};
+		vm.create = create;
+		vm.update = update;
+		vm.remove = remove;
 
-	    vm.create = function(){
+		activate();
+
+		function activate() {
+			vm.experiences = experienceService.query(); // returns all the pokemons
+		}
+
+	    function create() {
 	        experienceService.save(vm.newExperience);
+	        vm.experiences.push(vm.newExperience);
 	        vm.newExperience = {}; // clear new pokemon object
-	    };
+	    }
 
-	    vm.update = function(experience) {
+	    function update(experience) {
 	        experienceService.get({id: experience._id }, function() {
 	            experienceService.update({id: experience._id}, experience);
 	        });
-	    };
+	    }
 
-	    vm.delete = function(experience) {
+	    function remove(experience) {
 	        experienceService.remove({id: experience._id});
 	        var experienceIndex = vm.experiences.indexOf(experience);
 	        vm.experiences.splice(experienceIndex, 1);
-	    };
+	    }
 	}
 })();
