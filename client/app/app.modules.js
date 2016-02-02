909f9ba20278a8fc51ea45b2c460c1201df98da9
Paul Bottein <paul.bottein@gmail.com>
2016-02-02 16:59:13 +0100

    Work on slider (WIP)

--- client/app/app.modules.js
+++ client/app/app.modules.js
@@ -6,6 +6,7 @@
 			'ngResource',
 			'ui.router',
 			'ui.materialize',
+			'nouislider',
 			'app.controllers',
 			'app.filters',
 			'app.services',
@@ -27,4 +28,8 @@
 	/* Directives */
 	angular
 		.module('app.directives', []);
+
+	/* Redeclare ui.materialize module to exclude slider module (conflict with nouislider) */
+	angular.module("ui.materialize", ["ui.materialize.ngModel", "ui.materialize.collapsible", "ui.materialize.toast", "ui.materialize.sidenav", "ui.materialize.material_select", "ui.materialize.dropdown", "ui.materialize.inputfield", "ui.materialize.input_date", "ui.materialize.tabs", "ui.materialize.pagination", "ui.materialize.pushpin", "ui.materialize.scrollspy", "ui.materialize.parallax","ui.materialize.modal", "ui.materialize.tooltipped", "ui.materialize.materialboxed"]);
+
 })();
